d375cd20e925ed65fa701b47d2e6819c7cf98fb3
Victor Houston <victorhouston@gmail.com>
2014-08-04 20:54:42 +0100

    new

---
+++ src/com/bliksem/scientificunitconverter/LengthFragment.java
@@ -0,0 +1,69 @@
+package com.bliksem.scientificunitconverter;
+
+import java.util.ArrayList;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class LengthFragment extends Fragment {
+	
+	String unitGroup;
+
+	public LengthFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.length_fragment, container, false);
+		
+		Bundle bundle = this.getArguments();
+		
+	    unitGroup = bundle.getString("unit_group", "defaultValue");
+		
+		Toast.makeText(getActivity(), unitGroup, Toast.LENGTH_SHORT).show();
+		
+		return rootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		ArrayList<String> mLengthUnits = new ArrayList<String>();
+
+		String[] length_array = getResources().getStringArray(R.array.length_units);
+
+		for (int i = 0; i < length_array.length; ++i) {
+			mLengthUnits.add(length_array[i]);
+		}
+		
+		Spinner spinner = (Spinner) getView().findViewById(R.id.myspinner);
+
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> spinner_adapter = ArrayAdapter.createFromResource(getActivity(),
+				R.array.length_units, android.R.layout.simple_spinner_item);
+
+		// Specify the layout to use when the list of choices appears
+		spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(spinner_adapter);
+
+		StableArrayAdapter adapter = new StableArrayAdapter(getActivity().getApplicationContext(),
+				R.layout.listview_row, mLengthUnits);
+		DynamicListView listView = (DynamicListView) getView().findViewById(R.id.listview);
+
+		listView.setCheeseList(mLengthUnits);
+		listView.setAdapter(adapter);
+		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+	}
+}
